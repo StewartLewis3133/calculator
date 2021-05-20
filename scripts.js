a14ad6d222c2b10fd21fb6cf520aba400dc727f1
Stewart Lucas <cishcabob123@gmail.com>
2021-05-20 09:25:41 -0700

    Finished simple operator functionality.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-console.log('test');
+const buttonContainer = document.querySelector('#button-grid');
+
+function add (a, b) {
+	return (a + b);
+}
+
+function subtract (a,b) {
+	return (a - b);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(a, b) {
+    return (a / b);
+}
+
+function operate(a, b, operator) {
+    return (operator(a, b));
+}
