7a4e9cc8bdb4a37d983e5f4166afe845460c3534
Stewart Lucas <cishcabob123@gmail.com>
2021-05-24 14:16:37 -0700

    Calculator functionality is MVP I believe. Next to add keyboard support.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 const calcNumberButtons = document.querySelectorAll('.number-button');
 const calcOperatorButtons = document.querySelectorAll('.operator-button');
 const calcEqualsButton = document.querySelector('#equals-button');
+const calcClearButton = document.querySelector('#clear-button');
 let displayText = document.querySelector('#display-container');
 displayText.textContent = '';
+
 
 var displayObject = {
     firstInputNum: "",
@@ -13,12 +15,34 @@ var displayObject = {
 calcOperatorButtons.forEach(button => button.addEventListener('click', operatorFunction))
 calcNumberButtons.forEach(button => button.addEventListener('click', display));
 calcEqualsButton.addEventListener('click', finalOperate);
+calcClearButton.addEventListener('click', clearCalculator);
+
+function clearCalculator(){
+    displayObject.firstInputNum = '';
+    displayObject.secondInputNum = '';
+    displayObject.displayOperator = '';
+    displayText.textContent = '';
+}
 
 //Calculator's number buttons, allows user to type a full number
-//TODO: ALLOW DECIMAL INPUT
 function display(button) {
+    let num1 = displayObject.firstInputNum;
+    let num2 = displayObject.secondInputNum;
+    let nums = num1 + num2;
+    
+    if(nums.length > 28) {
+        return;
+    }
+    if(num1.indexOf('.') !== -1 && button.target.textContent === '.' && 
+        !displayObject.displayOperator){
+        return;
+    }
+    if (num2.indexOf('.') !== -1 && button.target.textContent === '.'){
+        return;
+    }
+
     displayText.textContent += button.target.textContent;
-    console.log(typeof button.target.textContent);
+
     if(displayObject.displayOperator){
         displayObject.secondInputNum += button.target.textContent;
     } else {
@@ -28,7 +52,6 @@ function display(button) {
 }
 
 //Fires on click of operator buttons and concatenates to the display
-//TODO: CHECK IF THERE ARE ALREADY TWO NUMBERS OR ALREADY AN OPERATOR
 function operatorFunction(button) {
     if(displayObject.displayOperator){
         return;
@@ -40,8 +63,27 @@ function operatorFunction(button) {
 
 //Fires on click of = button and calculates both input numbers, displaying the result
 function finalOperate() {
-    displayText.textContent = operate(displayObject.firstInputNum, 
-        displayObject.secondInputNum, window[displayObject.displayOperator]);
+    if (!displayObject.secondInputNum){
+        return;
+    }
+
+    if(displayObject.secondInputNum === "0" && displayObject.displayOperator === 'divide') {
+            displayText.textContent = 'So you thought you could beat me...';
+            return;
+        }
+    let num1 = Number(displayObject.firstInputNum);
+    let num2 = Number(displayObject.secondInputNum);
+
+    console.log(num1);
+    console.log(num2);
+
+    displayText.textContent = (operate(num1, num2, window[displayObject.displayOperator]));
+
+    displayObject.firstInputNum =  `${operate(num1, 
+        num2, window[displayObject.displayOperator])}`;
+
+    displayObject.secondInputNum = '';
+    displayObject.displayOperator = '';
 }
 
 //Operation Functions:
@@ -62,7 +104,7 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-    a = parseInt(a);
-    b = parseInt(b);
+    a = parseFloat(a);
+    b = parseFloat(b);
     return (operator(a, b));
 }
